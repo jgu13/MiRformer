fixed bugs in longformer cross attention masking

scripts/Pretrain.py
@@ -23,8 +23,8 @@ from Attention_regularization import kl_diag_seed_loss
 from Data_pipeline import CharacterTokenizer, QuestionAnswerDataset
 from utils import load_dataset
 
-PROJ_HOME = os.path.expanduser("~/projects/ctb-liyue/claris/projects/mirLM")
-# PROJ_HOME = os.path.expanduser("~/projects/mirLM")
+# PROJ_HOME = os.path.expanduser("~/projects/ctb-liyue/claris/projects/mirLM")
+PROJ_HOME = os.path.expanduser("~/projects/mirLM")
 # PROJ_HOME = os.path.expanduser("/Users/jiayaogu/Documents/Li Lab/mirLM---Micro-RNA-generation-with-mRNA-prompt/")
 
 def setup(rank, world_size):
@@ -703,7 +703,7 @@ def main():
             device = "mps"
             print("Using MPS (Apple Silicon GPU)")
         elif torch.cuda.is_available():
-            device = "cuda:0"
+            device = "cuda:1"
             print("Using CUDA GPU")
         else:
             device = "cpu"

scripts/multilevel_masking_pretraining.py
@@ -122,7 +122,8 @@ class TokenHead(torch.nn.Module):
         x = self.dense(hidden_states)                # (B, L, D)
         x = self.act(x)
         x = self.layer_norm(x)
-        logits = self.decoder(x) + self.bias         # (B, L, V)
+        x = self.decoder(x)
+        logits = x + self.bias         # (B, L, V)
         return logits
 
 class PairedPretrainWrapper(torch.nn.Module):
@@ -141,6 +142,15 @@ class PairedPretrainWrapper(torch.nn.Module):
         self.mirna_head = TokenHead(d_model, vocab_size, embed_weight=embed_weight)
 
     def forward_pair(self, mrna_ids, mrna_mask, mirna_ids, mirna_mask, attn_mask=None):
+        if self.base.predictor.use_longformer:
+            lf_mask = torch.where(
+                mrna_mask > 0,
+                torch.zeros_like(mrna_mask),  # Set all valid tokens to 0 (local attention)
+                torch.full_like(mrna_mask, fill_value=-1)  # Set original 0s (pads) to -1
+            )
+            # check lf_mask has all values smaller or equal to 0
+            assert (lf_mask <= 0).all(), "lf_mask has values greater than 0"
+
         # 1) encode separately
         mirna_sn_embedding = self.base.predictor.sn_embedding(mirna_ids)
         mrna_sn_embedding = self.base.predictor.sn_embedding(mrna_ids)
@@ -152,7 +162,7 @@ class PairedPretrainWrapper(torch.nn.Module):
         mrna_embedding      = mrna_sn_embedding + mrna_cnn_embedding # (batch_size, mrna_len, embed_dim)
 
         mirna_embedding = self.base.predictor.mirna_encoder(mirna_embedding, mask=mirna_mask)  # (batch_size, mirna_len, embed_dim)
-        mrna_embedding = self.base.predictor.mrna_encoder(mrna_embedding, mask=mrna_mask) # (batch_size, mrna_len, embed_dim)
+        mrna_embedding = self.base.predictor.mrna_encoder(mrna_embedding, mask=lf_mask) # (batch_size, mrna_len, embed_dim) use lf_mask for mrna_encoder
 
         # 2) cross-attn (sliding window) to enrich mRNA from miRNA
         ctx_mr, attn_weights = self.base.predictor.cross_attn_layer(
@@ -160,7 +170,7 @@ class PairedPretrainWrapper(torch.nn.Module):
                 key=mirna_embedding,
                 value=mirna_embedding,
                 attention_mask=mirna_mask,
-                query_attention_mask=mrna_mask,
+                query_attention_mask=mrna_mask, # use mask: 1=unmasked, 0=masked for cross attention
             )
         self.cross_attn_output = ctx_mr
         ctx_mr_res = self.base.predictor.dropout(ctx_mr) + mrna_embedding # residual connection
@@ -211,7 +221,7 @@ def loss_stage1_baseline(wrapper: PairedPretrainWrapper, batch, pad_id, mask_id,
     else:
         return total_loss
 
-def loss_stage2_seed_mrna(wrapper: PairedPretrainWrapper, batch, mask_id, vocab_size, evaluate=False):
+def loss_stage2_seed_mrna(wrapper: PairedPretrainWrapper, batch, pad_id, mask_id, vocab_size, evaluate=False):
     mi_in, mi_mask = batch["mirna_input_ids"], batch["mirna_attention_mask"]
     mr_in, mr_mask = batch["mrna_input_ids"],  batch["mrna_attention_mask"]
     seed_s, seed_e = batch["start_positions"], batch["end_positions"]  # (B,)
@@ -274,7 +284,7 @@ def run_pretrain_epoch(stage, wrapper, dataloader, optimizer, device,
     for batch in dataloader:
         for k in batch: batch[k] = batch[k].to(device)
         loss1 = loss_stage1_baseline(wrapper, batch, pad_id, mask_id)
-        loss2 = loss_stage2_seed_mrna(wrapper, batch, mask_id, vocab_size)
+        loss2 = loss_stage2_seed_mrna(wrapper, batch, pad_id, mask_id, vocab_size)
         loss3 = loss_stage3_bispan(wrapper, batch, pad_id, mask_id, vocab_size)
 
         loss = loss1 + loss2 + loss3

scripts/transformer_model.py
@@ -136,11 +136,15 @@ class LongformerAttention(nn.Module):
                 attention_mask=None,
                 query_attention_mask=None, # only used in cross attention when q != k 
                 output_attentions=False,):
-
-        bad_index = check_key_mask_rows(attention_mask)
-        print(f"bad index in {attention_mask.shape} attention mask: {bad_index}")
-
         if self.cross_attn:
+            if attention_mask is not None:
+                assert (attention_mask >= 0).all(), "attention_mask has values less than 0"
+                assert (attention_mask > 0).any(), "attention_mask has no values greater than 0"
+            if query_attention_mask is not None:
+                assert (query_attention_mask >= 0).all(), "query_attention_mask has values less than 0"
+                assert (query_attention_mask > 0).any(), "query_attention_mask has no values greater than 0"
+            check_key_mask_rows(attention_mask)
+
             bsz, q_len, _ = query.shape
             _, k_len, _   = key.shape
             _, v_len, _   = value.shape
@@ -163,14 +167,18 @@ class LongformerAttention(nn.Module):
             if query_attention_mask is not None:
                 assert attention_mask.shape == (bsz, k_len)
                 assert query_attention_mask.shape == (bsz, q_len)
+                
                 attention_mask = (attention_mask > 0)  # bool
                 query_attention_mask = (query_attention_mask > 0)  # bool
                 # Create mask for cross attention (B, L_q, L_k)
                 mask = attention_mask[:, None, :] & query_attention_mask[:, :, None]
                 mask = mask[:, None, :, :].expand(bsz, self.num_heads, q_len, k_len) # (B, H, L_q, L_k)
+            else:
+                # If no query attention mask, create a default mask that allows all attention
+                mask = torch.ones(bsz, self.num_heads, q_len, k_len, device=q.device, dtype=torch.bool)
 
             # check if the attention mask is all False
-            if mask.all() == False:
+            if not mask.any():
                 raise ValueError("Attention mask is all False")
 
             # 3) Sliding window cross attention for rest of query
@@ -203,6 +211,7 @@ class LongformerAttention(nn.Module):
             k = self.rotary(k)
             q /= math.sqrt(self.head_dim)
             if attention_mask is not None:
+                assert (attention_mask <= 0).all(), "attention_mask has values greater than 0"
                 key_padding_mask = attention_mask < 0
                 extra_attention_mask = attention_mask > 0
                 remove_from_windowed_attention_mask = attention_mask != 0
@@ -225,6 +234,7 @@ class LongformerAttention(nn.Module):
                     selection_padding_mask_nonzeros = selection_padding_mask.nonzero(as_tuple=True)
                     # 3) location of the padding values in the selected global attention
                     selection_padding_mask_zeros = (selection_padding_mask == 0).nonzero(as_tuple=True)
+                assert extra_attention_mask is None, "extra_attention_mask is not None"
             else:
                 remove_from_windowed_attention_mask = None
                 extra_attention_mask = None
@@ -626,7 +636,7 @@ class CrossAttentionPredictor(nn.Module):
     def __init__(self,  
                  mirna_max_len:int,
                  mrna_max_len:int, 
-                 vocab_size:int=13, # Fallback if tokenizer not provided (7 special + 1 MRNA_CLS + 5 bases)
+                 vocab_size:int=12, # Fallback if tokenizer not provided (7 special + 5 bases)
                  num_layers:int=2, 
                  embed_dim:int=256, 
                  num_heads:int=2, 
@@ -737,6 +747,8 @@ class CrossAttentionPredictor(nn.Module):
                 torch.zeros_like(mrna_mask),  # Set all valid tokens to 0 (local attention)
                 torch.full_like(mrna_mask, fill_value=-1)  # Set original 0s (pads) to -1
             )
+            # check lf_mask has all values smaller or equal to 0
+            assert (lf_mask <= 0).all(), "lf_mask has values greater than 0"
         
         # add N-gram CNN-encoded embedding
         mirna_cnn_embedding = self.cnn_embedding(mirna_sn_embedding.transpose(-1, -2)) # (batch_size, embed_dim, mirna_len)
@@ -746,6 +758,7 @@ class CrossAttentionPredictor(nn.Module):
         mirna_embedding = self.mirna_encoder(mirna_embedding, mask=mirna_mask)  # (batch_size, mirna_len, embed_dim)
 
         if self.use_longformer:
+            # use lf_mask for mrna_encoder
             mrna_embedding = self.mrna_encoder(mrna_embedding, mask=lf_mask) # (batch_size, mrna_len, embed_dim) with global attention
         else:
             mrna_embedding = self.mrna_encoder(mrna_embedding, mask=mrna_mask) # (batch_size, mrna_len, embed_dim)
@@ -756,7 +769,7 @@ class CrossAttentionPredictor(nn.Module):
                 key=mirna_embedding,
                 value=mirna_embedding,
                 attention_mask=mirna_mask,
-                query_attention_mask=lf_mask,  # Use Longformer mask for query (mRNA with global attention)
+                query_attention_mask=mrna_mask,  # Use mask: 1=unmasked, 0=masked for cross attention
             )[0]
             self.cross_attn_output = output
             z = output